8a27272cd424a9955bd7decb1c7b88cc75768dbc
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-06-14 13:27:03 +0500

    interested products added in product cart page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { client } from "../../lib/client";
 import { urlFor } from "../../lib/client";
 
-import Product from './../../components/Product';
+import Product from "./../../components/Product";
 
 import {
   AiOutlineMinus,
@@ -13,7 +13,8 @@ import {
 } from "react-icons/ai";
 
 const ProductDetails = ({ product, products }) => {
-  console.log("==>", product);
+  const [index, setIndex] = useState(0);
+
   const { image, name, details, price } = product;
   return (
     <div>
@@ -22,13 +23,15 @@ const ProductDetails = ({ product, products }) => {
           <div className="image-container">
             <img src={urlFor(image && image[0])} alt="" />
           </div>
-          {/* <div className='small-images-container'>
-                    {
-                        image ?.map((item,index) =>(
-                            <img src={urlFor(item)} className='' onMouseEnter=''/>
-                        ))
-                    }
-            </div> */}
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
         </div>
 
         <div className="product-detail-desc">
@@ -49,41 +52,38 @@ const ProductDetails = ({ product, products }) => {
           <p>{details}</p>
           <p className="price">${price}</p>
           <div className="quantity">
-              <h3>Quantity</h3>
-              <p className="quantity-desc">
-                  <span className="minus">
-                      <AiOutlineMinus/>
-                  </span>
-                  <span className="num">
-                    0
-                  </span>
-                  <span className="plus">
-                      <AiOutlinePlus/>
-                  </span>
-              </p>
+            <h3>Quantity</h3>
+            <p className="quantity-desc">
+              <span className="minus">
+                <AiOutlineMinus />
+              </span>
+              <span className="num">0</span>
+              <span className="plus">
+                <AiOutlinePlus />
+              </span>
+            </p>
           </div>
           <div className="buttons">
-               <button type="button" className="add-to-cart">Add to Cart</button>
-               <button type="button" className="buy-now">Buy Now</button>
-
+            <button type="button" className="add-to-cart">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now">
+              Buy Now
+            </button>
           </div>
         </div>
       </div>
 
-
-       <div className="maylike-products-warpper">
-            <h2>You may also like</h2>
-            <div className="marquee">
-                <div className="maylike-products-container track">
-                      {
-                          products.map((item,index) => (
-                              <Product key={item._id} product={item}/>
-                          ))
-                      }
-                </div>
-            </div>
-       </div>
-
+      <div className="maylike-products-warpper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item, index) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
